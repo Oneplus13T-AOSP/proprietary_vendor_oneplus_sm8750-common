abbdb4e07a520ab4f6fc2f52d0c4e66cf78b8082
RobertGarciaa <chae0218@naver.com>
2026-06-01 16:06:33 +0900

    up

--- sm8750-common-vendor.mk
+++ sm8750-common-vendor.mk
@@ -1257,8 +1257,6 @@ PRODUCT_PACKAGES += \
     vendor.qti.ImsRtpService-V2-ndk \
     vendor.qti.hardware.dpmaidlservice-V1-ndk \
     vendor.qti.hardware.dpmservice@1.0 \
-    vendor.qti.hardware.qteeconnector-V1-ndk \
-    vendor.qti.hardware.qteeconnector@1.0 \
     vendor.qti.ims.datachannelservice-V2-ndk \
     vendor.qti.ims.datachannelservice-V3-ndk \
     vendor.qti.imsrtpservice@3.0 \
