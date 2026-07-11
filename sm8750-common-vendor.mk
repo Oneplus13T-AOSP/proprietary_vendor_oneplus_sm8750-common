d2a6615aa0dc8c655b56f14a1c7c836fdc3a4f63
RobertGarciaa <chae0218@naver.com>
2026-07-11 15:42:15 +0900

    up

--- sm8750-common-vendor.mk
+++ sm8750-common-vendor.mk
@@ -680,6 +680,7 @@ PRODUCT_PACKAGES += \
     libwfduibcsinkinterface \
     libwfduibcsrc \
     libwfduibcsrcinterface \
+    vendor.oplus.hardware.charger-V11-ndk \
     vendor.qti.diaghal@1.0 \
     vendor.qti.hardware.qccsyshal@1.0 \
     vendor.qti.hardware.qccsyshal@1.1 \
